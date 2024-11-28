0a92577de9db95c6d3f1848cc387efb48ab492e4
TeaEdelborg <tea.e.edelborg@gmail.com>
2024-11-28 11:03:57 +0100

    selectedAnswer

--- src/components/QuestionComponent.vue
+++ src/components/QuestionComponent.vue
@@ -1,7 +1,16 @@
 <template>
 <p>{{question.q}}</p>
-<button v-for="a in question.a" v-on:click="answer(a)" v-bind:key="a">
+<button v-for="a in question.a" v-on:click="selectAnswer(a)" v-bind:key="a">
   {{ a }}
+</button>
+
+<!--
+<button v-bind:key="question.a[0]" v-on:click="selectAnswer(question.a[0])"> {{ question.a[0] }}</button>
+<button v-bind:key="question.a[1]" v-on:click="selectAnswer(question.a[1])"> {{ question.a[1] }}</button>
+<button v-bind:key="question.a[2]" v-on:click="selectAnswer(question.a[2])"> {{ question.a[2] }}</button>
+<button v-bind:key="question.a[3]" v-on:click="selectAnswer(question.a[3])"> {{ question.a[3] }}</button>-->
+<button v-on:click="answer">
+  Submitt answer
 </button>
 </template>
 <script>
@@ -10,11 +19,22 @@ export default {
   props: {
     question: Object
   },
+  data: function(){
+    return{
+      selectedAnswer:''
+    }
+  },
   emits: ["answer"],
   methods: {
-    answer: function (answer) {
-      this.$emit("answer", answer);
-    } 
+    answer: function () {
+      this.$emit("answer", this.selectedAnswer);
+    },
+    selectAnswer: function(answer){
+        console.log(answer)
+        this.selectedAnswer = answer
+    }
+
   }
 }
+//ha en "selected button, så om en knapp trycks på så väljs den knappen till selected, den ändrar också färg?"
 </script>
--- src/views/Join.vue
+++ src/views/Join.vue
@@ -1,3 +1,4 @@
 <template>
-    <!--Här ska vi lägga in att man joinar-->
+    <!--Här ska vi lägga in att man joinar, skicka med att man har gått med-->
+    <!--enklare att kanske endast ha lobbyView, kommer annars behöva skicka data emellan?-->
 </template>
