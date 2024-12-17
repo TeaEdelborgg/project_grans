e1495f763b5f2be09b9f093bc01f020b288e0638
TeaEdelborg <tea.e.edelborg@gmail.com>
2024-12-17 21:47:53 +0100

    css on result, button must be placed better

--- src/views/FinalResult.vue
+++ src/views/FinalResult.vue
@@ -1,5 +1,13 @@
 <template>
-    {{scoreBoard}}
+    <div id="background">
+        <div id="scoreBoard" v-if="answeresSentIn">
+            <h1>The winner is..</h1> <br>
+            <p>1. {{ winner.information.name }}</p> <br>
+            <h2>The losers are..</h2><br>
+            <p v-for="(player, index) in losers">{{ index + 2 }}. {{ player.information.name }}</p> <br>   
+            <button id="homeButtom" @click="$router.push('/')">Return to home page</button> 
+        </div>
+    </div>
 </template>
 
 <script>
@@ -16,7 +24,10 @@ export default {
     data: function () {
         return {
             pollId:"",
-            scoreBoard:[]
+            scoreBoard:[],
+            winner:"",
+            losers:[],
+            answeresSentIn:false
         }
     },
     created: function () {
@@ -25,7 +36,11 @@ export default {
         //this.pollId = this.$route.params.id;
         socket.on("sendScoreBoard", val=>{
             this.scoreBoard=val
-            console.log("tog emot scoreboard")
+            this.winner = val[0]
+            console.log(this.winner)
+            this.losers = val.slice(1,this.scoreBoard.length)
+            console.log(this.losers)
+            this.answeresSentIn=true
         })
         socket.on( "uiLabels", labels => this.uiLabels = labels );
     },
@@ -38,6 +53,35 @@ export default {
 </script>
 
 <style>
+#background{
+    width: 100vh;
+    height: 100vh;
+    background-color: #001F3F;
+}
+#scoreBoard{
+    height: 70%;
+    width: 80%;
+    background-color: #FF851B;
+    border-style: solid;
+    border-width: 10px;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    flex-grow: 1;
 
+}
+#homeButtom{
+    width: 100%;
+    margin-bottom: 10px;
+    cursor:pointer;
+    background-color: #39A2DB;
+    color:black;
+    height: 10%;
+}
+#homeButtom:hover{
+    background-color: #4b6ab8;
+    color: white;
+}
 
 </style>
