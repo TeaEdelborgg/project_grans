2787d64a1eb4c988b35f13e009580b08d44f79c5
beikr <beddra.ikriem@hotmail.com>
2024-12-29 10:53:49 +0100

    popup UI, kan inte join utan newpollid + style

--- src/views/Join.vue
+++ src/views/Join.vue
@@ -3,23 +3,20 @@
     <!--enklare att kanske endast ha lobbyView, kommer annars behöva skicka data emellan?-->
 
 <label>
-    Write poll id: 
-    <input type="text" v-model="newPollId">
+    <input type="text" v-model="newPollId" placeholder="Enter Poll ID">
   </label>
  <!-- <router-link v-bind:to="'/lobby/' + newPollId">
     {{ uiLabels.participatePoll }}
     <button>join</button>
   </router-link>
   <button v-on:click="checkPollStatus"> {{ uiLabels.participatePoll }} join</button>-->
-  
-  <router-link v-bind:to="'/lobby/' + newPollId">
-    {{ uiLabels.participatePoll }}
-    <button>join</button>
+  <transition name="fade">
+  <router-link v-if="newPollId" v-bind:to="'/lobby/' + newPollId" class="joinButton">
+    <button>
+      {{ uiLabels.participatePoll }} Join
+    </button>
   </router-link>
-
-
-        
-   
+  </transition>
 </template>
 
 <script>
@@ -52,3 +49,62 @@ export default {
   }
 }
   </script>
+
+<style scoped>
+
+input[type="text"] {
+  margin: 20px auto; 
+  margin-top: 140px;
+  padding: 15px; 
+  font-size: 30px; 
+  text-align: center;
+  border: 2px solid #000000;
+  border-radius: 30px; 
+  width: 30%; 
+  height: 50px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
+  background-color: #ffffff;
+}
+
+input[type="text"]:focus::placeholder {
+  opacity: 0; 
+  transition: opacity 0.1s ease-in-out;
+}
+
+input[type="text"]:hover {
+  background-color: #e7e7e7;
+  transition: background-color 0.3s ease;
+}
+
+button {
+  display: block;
+  margin: 30px auto; 
+  padding: 15px 40px; 
+  font-size: 18px; 
+  background-color: rgb(18, 150, 22);
+  color: #fff; 
+  border: none;
+  border-radius: 5px; 
+  cursor: pointer;
+  box-shadow: 0 8px 6px rgba(0, 0, 0, 0.1); 
+  transition: background-color 0.3s ease; 
+  width: 220px;
+  text-decoration: none;
+}
+
+button:hover {
+  background-color: rgb(16, 115, 20); 
+  box-shadow: 0 8px 6px rgba(0, 0, 0, 0.4); 
+}
+.joinButton {
+  text-decoration: none;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/views/LobbyView.vue
+++ src/views/LobbyView.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    {{ pollId }}
     <div v-if="!joined">
       <input type="text" v-model="userName" placeholder="Enter your name" />
-
+      <h1>Pick a color:</h1>
       <div>
         <ul class="colorPicker">
           <li v-for="color in colors" :key="color.hex">
             <a
               href="#"
               :style="{ background: color.hex }"
-              v-on:click="setColor(color.hex)"
+              v-on:click="setColor(color.hex, $event)"
               :class="{ selected: selectedColor === color.hex, disabled: isColorDisabled(color.hex) }">
             </a>
           </li>
@@ -18,9 +17,16 @@
         <input type="hidden" v-model="selectedColor" />
       </div>
 
-      <button v-on:click="participateInPoll" :disabled="!userName || isColorDisabled(selectedColor) || selectedColor ==='#ff0000'">
-        {{ uiLabels.participateInPoll || "Join Poll" }}
-      </button>
+      <div class="participateButton">
+        <button v-on:click="participateInPoll" :disabled="!userName || isColorDisabled(selectedColor) || selectedColor ==='#ff0000'">
+          {{ uiLabels.participateInPoll || "Join Poll" }}
+        </button>
+      </div>
+
+      <div v-if="showPopup" class="popup">
+        <p>Some colors are already taken. Hurry up and pick you favourite!</p>
+        <button @click="closePopup">X</button>
+      </div>
     </div>
 
     <div v-if="joined">
@@ -60,6 +66,8 @@ export default {
         {hex: "#008B8B"},
         {hex: "#4682B4"},
       ],
+      showPopup: false,
+      popupShown: false,
     };
   },
   created() {
@@ -67,13 +75,15 @@ export default {
     socket.on("uiLabels", (labels) => (this.uiLabels = labels));
     socket.on("participantsUpdate", (p) => {
       this.participants = p;
+      this.checkForDisabledColors(); 
     });
     socket.on("startPoll", () =>
       this.$router.push("/poll/" + this.pollId + "/" + this.userID)
     );
     socket.on("colorSelectionUpdate", (updatedParticipants) => {
-      this.participants=updatedParticipants;
-    })
+      this.participants = updatedParticipants;
+      this.checkForDisabledColors();
+    });
     socket.emit("joinPoll", this.pollId);
     socket.emit("getUILabels", this.lang);
   },
@@ -102,22 +112,42 @@ export default {
         (participant) => participant.information.color === color
       );
     },
-}};
+    checkForDisabledColors() {
+      if (!this.popupShown && this.colors.some(color => this.isColorDisabled(color.hex))) {
+        this.showPopup = true; 
+        this.popupShown = true; 
+      }
+    },
+    closePopup() {
+      this.showPopup = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
 .colorPicker {
   list-style: none;
   display: flex;
-  gap: 5px;
+  gap: 15px;
+  justify-content: center; 
+  margin-top: 20px;
+  margin-left: -30px;
 }
 
 .colorPicker li a {
   display: block;
-  height: 30px;
-  width: 30px;
-  border: 2px solid transparent;
+  height: 60px;
+  width: 60px;
+  border: 3px solid transparent;
   cursor: pointer;
+  border-radius: 40%;
+}
+
+.colorPicker li a:hover {
+  /*border-color: #000;*/
+  transform: scale(1.2);
+  transition: transform 0.2s ease;
 }
 
 .colorPicker li a.selected {
@@ -125,7 +155,114 @@ export default {
 }
 
 .colorPicker li a.disabled {
-  opacity: 0.5;
+  opacity: 0.4;
   pointer-events: none;
+  position: relative;
+  transform: scale(0.7);
+  border: none;
 }
+
+input[type="text"] {
+  margin: 40px auto; 
+  margin-top: 110px;
+  padding: 15px; 
+  font-size: 30px; 
+  text-align: center;
+  border: 2px solid #000000;
+  border-radius: 40px; 
+  width: 35%; 
+  height: 50px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
+  background-color: #ffffff;
+}
+
+input[type="text"]:focus::placeholder {
+  opacity: 0; 
+  transition: opacity 0.1s ease-in-out;
+}
+
+input[type="text"]:hover {
+  background-color: #e7e7e7;
+  transition: background-color 0.3s ease;
+}
+
+.participateButton button {
+  display: block;
+  margin: 30px auto; 
+  padding: 15px 40px; 
+  font-size: 18px; 
+  background-color: rgb(18, 150, 22);
+  color: #fff; 
+  border: none;
+  border-radius: 5px; 
+  cursor: pointer;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
+  transition: background-color 0.3s ease; 
+  width: 220px;
+}
+
+.participateButton button:hover:not(:disabled) {
+  background-color: rgb(16, 115, 20); 
+  box-shadow: 0 8px 6px rgba(0, 0, 0, 0.4); 
+}
+.participateButton button:disabled {
+  filter: white;
+  cursor: default; 
+  opacity: 50%;
+}
+
+@keyframes slideIn {
+  0% {
+    top: -80px; 
+  }
+  100% {
+    top: 2%;
+  }
+}
+.popup {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  background: rgb(192, 5, 5);
+  padding: 5px;
+  box-shadow: 0 8px 10px rgba(0, 0, 0, 0.4);
+  animation: slideIn 0.5s ease-in-out forwards; 
+  border-radius: 10px;
+  width: 250px;
+  height: 60px;
+}
+.popup p{
+  font-size: 15px;
+  padding-right: 24px;
+  position: absolute;
+  top: 2px;
+}
+.popup button{
+  background: rgb(192, 5, 5);
+  cursor: pointer;
+  position: absolute; 
+  top: 9px; 
+  right: 10px; 
+}
+
+h1 {
+  font-size: 20px;
+}
+
+
+/*
+.colorPicker li a.selected {
+  border-color: #000;
+  transform: scale(1.2);
+}
+.colorPicker li a.disabled::after {
+  opacity: 1; 
+  content: '✖'; 
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 35px; 
+  color: rgb(214, 15, 15); 
+}*/
 </style>
