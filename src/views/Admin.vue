8c2447b6689bb4ec446bc9c35d2bc613fe6d55bd
manda1214 <amandanilsson02@gmail.com>
2025-01-09 19:52:28 +0100

    admin css players changed, end game popup added

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -10,7 +10,7 @@
       <div class="questions">
         <button class="game-button" @click="toggleQuestions">Visa frågor och svar</button>
         <div class="popup" v-if="showQuestions" @click.self="toggleQuestions">
-          <div class="popup-content">
+          <div class="popup-questions-content">
             <button class="close-button" @click="toggleQuestions">x</button>
             <h3>Frågor och svar</h3>
             <li v-for="(question, index) in pollData.questions" :key="index">
@@ -25,7 +25,8 @@
 
     <section class="game-controls">
       <h2>Styr spelet</h2>
-      <button class="game-button" v-if="pollData.currentQuestion + 1 != pollData.questionAmount && canStartNextQuestion" 
+      <button class="game-button" v-if="pollData.currentQuestion == -1" @click="runQuestion">Starta Spel</button>
+      <button class="game-button" v-else-if="pollData.currentQuestion + 1 != pollData.questionAmount && canStartNextQuestion" 
         @click="runQuestion">
         Nästa fråga
       </button>
@@ -42,22 +43,31 @@
     <section class="participant-controls"> <!-- lägga in för att kunna ta bort spelare här? -->
       <h2>Spelare</h2>
       <div class="participants"> 
-        <div v-for="(participant, index) in pollData.participants" :key="index">
-          <div class="one-participant"> 
-            <strong>{{ participant.information.name }}</strong> <!-- sätta som p ist? -->
-            <div v-if="participant.information.answers?.[pollData.currentQuestion]?.[0] == null">
-              Svarade: <div class="animatedDots"></div><div class="animatedDots"></div><div class="animatedDots"></div>
+        <div class="one-participant" v-for="(participant, index) in pollData.participants" :key="index"> 
+          <p class="player-name">{{ participant.information.name }}</p> <!-- sätta som p ist? -->
+          <hr>
+          <div v-if="pollData.currentQuestion == -1">Answer</div>
+          <div v-else-if="participant.information.answers?.[pollData.currentQuestion]?.[0] == null">
+            <div class="loading-dots">
+              <div class="animatedDots"></div><div class="animatedDots"></div><div class="animatedDots"></div>
             </div>
-            <div v-else>
-              Svarade: {{ participant.information.answers[pollData.currentQuestion][0] || "Har inte svarat ännu" }}
-            </div>
+          </div>
+          <div v-else>
+            {{ participant.information.answers[pollData.currentQuestion][0] || "Har inte svarat ännu" }}
           </div>
         </div>
       </div>
     </section>
 
     <section class="end-game">
-      <button class="game-button" v-on:click="finishGame()">Avsluta spelet tidigare?</button>
+      <button class="game-button" @click="toggleEndGame">Avsluta spelet tidigare?</button>
+      <div class="popup" v-if="showEndGame" @click.self="toggleEndGame">
+        <div class="popup-end-game-content">
+          <p>Är du säker?</p>
+          <button class="continue-button" @click="toggleEndGame">fortsätt spel</button>
+          <button class="end-button" @click="finishGame">avsluta spel</button>
+        </div>
+      </div>
     </section>
     
     <router-link v-bind:to="'/result/' + pollId">Check result</router-link>
@@ -83,6 +93,7 @@ export default {
       numberPlayersAnswered: 0, 
       canStartNextQuestion: true,
       showQuestions: false,
+      showEndGame: false,
     }
   },
   created: function () {
@@ -146,12 +157,16 @@ export default {
       console.log(this.pollData.participants)
       setTimeout(()=>{
         socket.emit('clearParticipants', this.pollId)
+        this.$router.push('/') // eller skickas någon annanstans? hur ska man göra? idk
         console.log('participants cleared')
       }, 2000)
     },
     toggleQuestions() {
       this.showQuestions = !this.showQuestions;
     },
+    toggleEndGame() {
+      this.showEndGame = !this.showEndGame
+    }
   }
 }
 </script>
@@ -159,12 +174,10 @@ export default {
 <style scoped>
 .admin {
   position: relative;
-  min-height: 86vh;
-  margin: 1%;
+  min-height: 90.7vh;
   color: #cfcfcf;
   background: radial-gradient(#1a237e, #0d1137);;
   padding: 2%;
-  border-radius: 10px;
   box-shadow: 0 4px 20px rgba(0, 0, 0, 0.4);
 }
 h1 {
@@ -188,10 +201,10 @@ h2 {
 }
 .game-button {
   align-items: center;
-  background-image: linear-gradient(135deg, #fF6E00 40%, #FF8800);
+  background-image: linear-gradient(135deg, #ff8800 40%, #ff9d00);
   border: 1%; 
   border-radius: 10px;
-  border-color: #FF8800;
+  border-color: #d97400;
   color: #eae9e9;
   cursor: pointer;
   font-size: 90%;
@@ -201,7 +214,7 @@ h2 {
   text-transform: uppercase;
 }
 .game-button:hover:enabled {
-  background-image: linear-gradient(135deg, #fF6E00 40%, #FF8800);
+  background-image: linear-gradient(135deg, #d16f00 40%, #dd8800);
   border-color: #bd782a;
   color: #d6b79f
 }
@@ -232,30 +245,69 @@ h2 {
   z-index: 1000;
   overflow: hidden; 
 }
-.popup-content {
+.popup-end-game-content {
+  position: relative;
+  background: #0d1137;
+  color: #c7c7c7;
+  padding: 20px;
+  border-radius: 10px;
+  width: 15%; 
+  max-height: 80vh;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.4); 
+}
+.continue-button {
+  background: #4caf50;
+  color: #ffffff;
+  border-radius: 8px;
+  border: none;
+  padding: 0.5em 1em;
+  margin:0.5em;
+  font-size: 1em;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+}
+.continue-button:hover {
+  background: #408b44;
+}
+.end-button {
+  background: #f44336;
+  color: #ffffff;
+  border-radius: 8px;
+  border: none;
+  padding: 0.5em 1em;
+  margin:0.5em;
+  font-size: 1em;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+}
+.end-button:hover {
+  background: #c62828;
+}
+.popup-questions-content {
   position: relative;
   background: #0d1137;
   color: #c7c7c7;
   padding: 20px;
   border-radius: 10px;
   width: 90%; 
-  max-width: 600px; 
   max-height: 80vh;
   overflow-y: auto; 
   box-shadow: 0 4px 20px rgba(0, 0, 0, 0.4); 
 }
 /* till scrollbar */
-.popup-content::-webkit-scrollbar { /* självaste scrollbaren */
+.popup-questions-content::-webkit-scrollbar { /* självaste scrollbaren */
   width: 10px;
 }
-.popup-content::-webkit-scrollbar-thumb { /* det som man drar i */
+.popup-questions-content::-webkit-scrollbar-thumb { /* det som man drar i */
   background: #2e2e2e;
   border-radius: 10px;
 }
-.popup-content::-webkit-scrollbar-thumb:hover {
+.popup-questions-content::-webkit-scrollbar-thumb:hover {
   background: #212121;
 }
-.popup-content::-webkit-scrollbar-track { /* det som den ligger i */
+.popup-questions-content::-webkit-scrollbar-track { /* det som den ligger i */
   background: #565656;
   border-radius: 10px;
 }
@@ -285,18 +337,25 @@ h2 {
 .participants {
   display: flex;
   justify-content: center;
+  align-items: center;
   flex-wrap: wrap;
-  gap: 5%;
 }
 .one-participant {
-  background: #2e2e2e;
-  padding: 5%;
-  margin: 3%;
-  border-radius: 5px;
+  background: #0c0f38;
+  padding: 1.5em;
+  margin: 1em;
+  border-radius: 10px;
   color: #cfcfcf;
   text-align: center;
-  width: 12vw; 
-  min-height: 11vh;
+  width: 10vw; 
+  min-height: 13vh;
+}
+.player-name {
+  font-size: 1.2em;
+  font-weight: bold;
+  color: #cfcfcf;
+  margin-bottom: 0.5em;
+  margin-top: 0;
 }
 
 /* animering av prickar nedan */
@@ -311,15 +370,15 @@ h2 {
 @keyframes dotsAnimation {
   0%, 100%{
     transform: scale(0.2);
-    background-color: #30FFB7;
+    background-color: #e4e4e4;
   }
   40%{
     transform: scale(1);
-    background-color: #07DEFF;
+    background-color: #7a7a7a;
   }
   50%{
     transform: scale(1);
-    background-color: #0761FF;
+    background-color: #484848;
   }
 }
 .animatedDots:nth-child(0){
